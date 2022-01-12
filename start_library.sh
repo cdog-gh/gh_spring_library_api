7b5de25057fd19424f2ca148038963c5af819647
chogahui <chogahui05@naver.com>
2022-01-13 00:20:02 +0900

    start.sh modify
    start.sh 수정

--- start_library.sh
+++ start_library.sh
@@ -1,5 +1,5 @@
-mv application.properties library/target/application.properties
-cd library/target
+mv ./application.properties /library/target/application.properties
+cd /library/target
 keytool -genkey -alias spring -storetype PKCS12 -keystore keystore.p12 -keyalg RSA -keysize 2048 \
         -validity 10000 -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown" \
         -storepass $KEY_PASSWORD -keypass $KEY_PASSWORD
